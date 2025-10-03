0d77cd23fbe97a85cf046c520aa38e59de510e2e
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-10-03 12:18:06 +0530

    feat(TechSpardha): Event Outline added

---
+++ apps/web-react/src/components/Events/EventCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EventCard() {
+  return (
+    <div>
+      This is event card
+    </div>
+  )
+}
+
+export default EventCard
---
+++ apps/web-react/src/components/Home/Event/Event.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventCard from '../../Events/EventCard.jsx'
+function Event() {
+  return (
+    <div>
+      <EventCard/>
+      <EventCard/>
+    </div>
+  )
+}
+
+export default Event
