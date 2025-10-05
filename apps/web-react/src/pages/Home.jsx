5a4166a20b2d5bced3d5cb7e7c83605957b4140d
akash-912 <akashdeep12896@gmail.com>
2025-10-05 22:43:03 +0530

    feat(Techspardha): Created the FAQ section for the home page

--- apps/web-react/src/pages/Home.jsx
+++ apps/web-react/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Carousel from '../components/Home/Guests/Carousel'
 import Event from '../components/Home/Event/Event'
 import Developer from './Developer'
 import CountdownTimer from '../components/Home/Timer/CountdownTimer.jsx'; 
+import FAQ from '../components/Home/FAQ/FAQ.jsx'
 function Home() {
   const cards=[{name:'Salman Khan',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'},{name:'Shah Rukh Khan',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'},{name:'Amitabh Bacchan',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'},{name:'Deepika Padukone',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'},{name:'Ananya Pandey',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'},{name:'Saif Ali Khan',imageUrl:'https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg?w=414&dpr=1.0&q=80',description:'Salman Khan is a renowned actor in Bollywood Industry famous for his films like Bajrangi bhaijan,Tubelight etc.'}]
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Carousel items={cards}/>
       <Event/>
       <Developer/>
+      <FAQ/>
     </div>
   )
 }
